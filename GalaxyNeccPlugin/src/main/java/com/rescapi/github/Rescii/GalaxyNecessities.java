590f480f15b4ea504996fd86ead09910ae749264
BuildTools <unconfigured@null.spigotmc.org>
2022-05-21 14:30:11 -0600

    Made some changes Added a ReportIssue command Added join text

--- GalaxyNeccPlugin/src/main/java/com/rescapi/github/Rescii/GalaxyNecessities.java
+++ GalaxyNeccPlugin/src/main/java/com/rescapi/github/Rescii/GalaxyNecessities.java
@@ -1,6 +1,7 @@
 package com.rescapi.github.Rescii;
 
 import com.rescapi.github.Rescii.events.MinigameEvents;
+import com.rescapi.github.Rescii.events.NormalEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +12,7 @@ public class GalaxyNecessities extends JavaPlugin {
         Bukkit.getLogger().info("Registering events for Necessities.");
 
         getServer().getPluginManager().registerEvents(new MinigameEvents(), this);
+        getServer().getPluginManager().registerEvents(new NormalEvents(), this);
 
         Bukkit.getLogger().info("Necessities event registering finished.");
     }
